c7968a30b53e6bff892cf58169835fafe3f39e76
Timothée Desveaux <timothee.desveaux@epita.fr>
2021-07-29 14:42:43 +0200

    add custom icons for diagnostics

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -38,3 +38,16 @@ local servers = { "pyright", "clangd" }
 for _, lsp in ipairs(servers) do
     require('lspconfig')[lsp].setup { on_attach = documentHighlight }
 end
+
+require('lspsaga').init_lsp_saga {
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "",
+    infor_sign = "",
+    code_action_icon = "",
+    code_action_prompt = {
+        enable = true,
+        sign = false,
+        virtual_text = true
+    }
+}
