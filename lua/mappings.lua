1a979a942e4473df7fbec516c45937ce56065494
Timothée Desveaux <timothee.desveaux@gmail.com>
2021-09-30 14:10:09 +0200

    add bindings to move around tabs

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ util.nnoremap("Y", "y$")
 util.nnoremap("<leader><leader>", ":e #<CR>")
 util.vnoremap("J", ":m '>+1<CR>gv=gv")
 util.vnoremap("K", ":m '<-2<CR>gv=gv")
+util.nnoremap("<leader>j", "gT")
+util.nnoremap("<leader>k", "gt")
 vim.cmd [[
 autocmd FileType markdown nnoremap <buffer> <leader>mp :silent !pandoc % -o %:r.pdf<CR>
 autocmd FileType markdown nnoremap <buffer> <leader>mv :silent !zathura %:r.pdf &<CR>
