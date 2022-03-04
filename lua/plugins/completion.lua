d1423fcd71b42401b6641fd8748f977f8c30f40e
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-03-04 20:39:12 +0100

    do not try to complete after word

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,3 @@
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0
-        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-                :sub(col, col)
-                :match("%s")
-            == nil
-end
-
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 
@@ -34,8 +25,6 @@ cmp.setup({
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
