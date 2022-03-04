8d603c8f6da37d6bfabd05c40b9a042c1c1f4bcf
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-03-04 15:38:06 +0100

    change completion and snippets mappings

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,4 +1,3 @@
-local util = require("utils")
 local luasnip = require("luasnip")
 
 luasnip.config.set_config({
@@ -7,17 +6,3 @@ luasnip.config.set_config({
 })
 
 require("luasnip/loaders/from_vscode").load()
-
-util.smap("<C-l>", "<cmd>lua require('luasnip').jump(1)<CR>", {
-    silent = true,
-})
-util.imap("<C-l>", "<cmd>lua require('luasnip').jump(1)<CR>", {
-    silent = true,
-})
-
-util.smap("<C-h>", "<cmd>lua require('luasnip').jump(-1)<CR>", {
-    silent = true,
-})
-util.imap("<C-h>", "<cmd>lua require('luasnip').jump(-1)<CR>", {
-    silent = true,
-})
