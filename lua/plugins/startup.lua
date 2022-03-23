a505bd85cbcf4ad3de415dde2051556993dd6559
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-03-23 17:46:46 +0100

    fix settings in alpha

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -15,11 +15,11 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button("e", "  > New file", ":ene <CR>"),
+    dashboard.button("e", "  > New file", ":ene<CR>"),
     dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
     dashboard.button("r", "  > Find word", ":Telescope live_grep<CR>"),
     dashboard.button("t", "פּ  > File explorer", ":NvimTreeOpen<CR>"),
-    dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h"),
+    dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h<CR>"),
     dashboard.button("u", "  > Update plugins", ":PackerUpdate<CR>"),
     dashboard.button("g", "  > Neogit", ":Neogit<CR>"),
     dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
