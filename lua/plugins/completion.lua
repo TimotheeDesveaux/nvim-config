bd46d6d99150100bdf3933ef9c01ae94bce52496
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-05-04 22:46:27 +0200

    cmp: correctly set mappings and separate source with fallbacks

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,20 +6,10 @@ cmp.setup({
             require("luasnip").lsp_expand(args.body)
         end,
     },
-    sources = {
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "calc" },
-        { name = "nvim_lsp_signature_help" },
-    },
-    mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
+    mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -38,29 +28,43 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Replace,
             select = false,
         }),
-    },
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lua" },
+    }, {
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "luasnip" },
+        { name = "calc" },
+        { name = "nvim_lsp_signature_help" },
+    }, {
+        { name = "buffer" },
+    }),
     formatting = {
         format = require("lspkind").cmp_format({
             with_text = true,
             menu = {
-                buffer = "[buf]",
-                nvim_lsp = "[LSP]",
                 nvim_lua = "[api]",
+                nvim_lsp = "[LSP]",
                 path = "[path]",
                 luasnip = "[snip]",
                 calc = "[calc]",
+                nvim_lsp_signature_help = "[sign]",
+                buffer = "[buf]",
             },
         }),
     },
 })
 
 cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = "buffer" },
     },
 })
 
 cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
         { name = "path" },
     }, {
