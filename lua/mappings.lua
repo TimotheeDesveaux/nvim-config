d25e152a2cec5240472d226cc878e99dbc2c9184
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-10-23 19:24:18 +0200

    mappings: delete v_J and v_K mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,6 @@
 local wk = require("which-key")
 
 vim.g.mapleader = " "
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-k>", "<c-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
