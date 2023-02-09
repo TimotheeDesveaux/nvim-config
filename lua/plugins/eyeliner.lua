41ec66eb2e15e7fc9421a12a5954dc784c1974c0
Timothée Desveaux <timothee.desveaux@epita.fr>
2023-02-09 13:09:38 +0100

    eyeliner: use dim option instead of underline

--- lua/plugins/eyeliner.lua
+++ lua/plugins/eyeliner.lua
@@ -1,6 +1,4 @@
-vim.api.nvim_set_hl(0, "EyelinerPrimary", { bold = true, underline = true })
-vim.api.nvim_set_hl(0, "EyelinerSecondary", { underline = true })
-
 require("eyeliner").setup({
     highlight_on_key = true,
+    dim = true,
 })
