194b4cbbb6dbc9593c21155f78e7a31c1b7a5920
Timothee Desveaux <timothee.desveaux@gmail.com>
2023-04-04 11:57:44 +0200

    snippets: format

--- lua/plugins/snippets/init.lua
+++ lua/plugins/snippets/init.lua
@@ -5,43 +5,43 @@ return {
         update_events = { "TextChanged", "TextChangedI" },
     },
     keys = {
-            {
-                "<C-j>",
-                function()
-                    if require("luasnip").jumpable(1) then
-                        require("luasnip").jump(1)
-                    end
-                end,
-                mode = { "i", "s" },
-            },
-            {
-                "<C-k>",
-                function()
-                    if require("luasnip").jumpable(-1) then
-                        require("luasnip").jump(-1)
-                    end
-                end,
-                mode = { "i", "s" },
-            },
-            {
-                "<C-l>",
-                function()
-                    if require("luasnip").choice_active() then
-                        require("luasnip").change_choice(1)
-                    end
-                end,
-                mode = { "i", "s" },
-            },
-            {
-                "<C-h>",
-                function()
-                    if require("luasnip").choice_active() then
-                        require("luasnip").change_choice(-1)
-                    end
-                end,
-                mode = { "i", "s" },
-            },
+        {
+            "<C-j>",
+            function()
+                if require("luasnip").jumpable(1) then
+                    require("luasnip").jump(1)
+                end
+            end,
+            mode = { "i", "s" },
         },
+        {
+            "<C-k>",
+            function()
+                if require("luasnip").jumpable(-1) then
+                    require("luasnip").jump(-1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-l>",
+            function()
+                if require("luasnip").choice_active() then
+                    require("luasnip").change_choice(1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-h>",
+            function()
+                if require("luasnip").choice_active() then
+                    require("luasnip").change_choice(-1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+    },
     config = function(_, opts)
         require("luasnip").setup(opts)
 
