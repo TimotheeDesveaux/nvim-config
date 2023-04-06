74e1aa53a31c953e75c52f5df2f72f70309298f0
Timothee Desveaux <timothee.desveaux@gmail.com>
2023-04-06 11:54:50 +0200

    keymaps: center screen after jumping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@ vim.keymap.set("n", "<leader><leader>", ":e #<CR>", { desc = "previous file" })
 vim.keymap.set("n", "<leader>j", "gT", { desc = "previous tab" })
 vim.keymap.set("n", "<leader>k", "gt", { desc = "next tab" })
 
+-- Center after jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+
 -- Move to window
 vim.keymap.set("n", "<C-k>", "<c-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
