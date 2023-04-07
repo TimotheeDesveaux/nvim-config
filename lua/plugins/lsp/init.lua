10233cc571929dbac9a39c3344ef7f3c105ad133
Timothee Desveaux <timothee.desveaux@gmail.com>
2023-04-07 14:14:15 +0200

    lsp(ui): move ui config to separate file

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -18,27 +18,7 @@ return {
         },
         event = { "BufReadPre", "BufNewFile" },
         config = function()
-            local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-            end
-            local border = {
-                { "╭", "FloatBorder" },
-                { "─", "FloatBorder" },
-                { "╮", "FloatBorder" },
-                { "│", "FloatBorder" },
-                { "╯", "FloatBorder" },
-                { "─", "FloatBorder" },
-                { "╰", "FloatBorder" },
-                { "│", "FloatBorder" },
-            }
-            local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-            function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-                opts = opts or {}
-                opts.border = opts.border or border
-                return orig_util_open_floating_preview(contents, syntax, opts, ...)
-            end
+            require("plugins.lsp.ui").setup()
 
             local function on_attach(client, bufnr)
                 require("plugins.lsp.keymaps").attach(bufnr)
