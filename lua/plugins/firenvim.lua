d869c18d0f9cabaa5a146fce258ed22eb681b64f
Timothee Desveaux <timothee.desveaux@gmail.com>
2023-04-17 13:48:56 +0200

    plugins: return the plugin spec directly when there is only one

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,30 +1,28 @@
 return {
-    {
-        "glacambre/firenvim",
-        build = function()
-            require("lazy").load({ plugins = { "firenvim" }, wait = true })
-            vim.fn["firenvim#install"](0)
-        end,
-        cond = not not vim.g.started_by_firenvim,
-        config = function()
-            vim.g.firenvim_config = {
-                globalSettings = {
-                    alt = "all",
+    "glacambre/firenvim",
+    build = function()
+        require("lazy").load({ plugins = { "firenvim" }, wait = true })
+        vim.fn["firenvim#install"](0)
+    end,
+    cond = not not vim.g.started_by_firenvim,
+    config = function()
+        vim.g.firenvim_config = {
+            globalSettings = {
+                alt = "all",
+            },
+            localSettings = {
+                [".*"] = {
+                    cmdline = "neovim",
+                    content = "text",
+                    priority = 0,
+                    selector = "textarea",
+                    takeover = "never",
                 },
-                localSettings = {
-                    [".*"] = {
-                        cmdline = "neovim",
-                        content = "text",
-                        priority = 0,
-                        selector = "textarea",
-                        takeover = "never",
-                    },
-                },
-            }
+            },
+        }
 
-            vim.opt.laststatus = 0
-            vim.opt.cmdheight = 0
-            vim.opt.signcolumn = "auto"
-        end,
-    },
+        vim.opt.laststatus = 0
+        vim.opt.cmdheight = 0
+        vim.opt.signcolumn = "auto"
+    end,
 }
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,93 +1,91 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
-            {
-                "folke/neodev.nvim",
-                config = true,
-            },
-            {
-                "kosayoda/nvim-lightbulb",
-                opts = {
-                    sign = { enabled = false },
-                    virtual_text = {
-                        enabled = true,
-                        text = "",
-                    },
-                    autocmd = { enabled = true },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        { "hrsh7th/cmp-nvim-lsp" },
+        {
+            "folke/neodev.nvim",
+            config = true,
+        },
+        {
+            "kosayoda/nvim-lightbulb",
+            opts = {
+                sign = { enabled = false },
+                virtual_text = {
+                    enabled = true,
+                    text = "",
                 },
+                autocmd = { enabled = true },
             },
-            { "SmiteshP/nvim-navic" },
-            {
-                "j-hui/fidget.nvim",
-                opts = {
-                    text = {
-                        spinner = "dots",
-                        done = "",
-                    },
+        },
+        { "SmiteshP/nvim-navic" },
+        {
+            "j-hui/fidget.nvim",
+            opts = {
+                text = {
+                    spinner = "dots",
+                    done = "",
                 },
             },
         },
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("plugins.lsp.ui").setup()
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        require("plugins.lsp.ui").setup()
 
-            vim.diagnostic.config({ severity_sort = true })
+        vim.diagnostic.config({ severity_sort = true })
 
-            local function on_attach(client, bufnr)
-                require("plugins.lsp.keymaps").attach(bufnr)
+        local function on_attach(client, bufnr)
+            require("plugins.lsp.keymaps").attach(bufnr)
 
-                if client.server_capabilities.documentSymbolProvider then
-                    require("nvim-navic").attach(client, bufnr)
-                    vim.opt_local.winbar = "%{%v:lua.require('nvim-navic').get_location()%}"
-                end
-
-                -- Highlight references
-                if client.server_capabilities.documentHighlightProvider then
-                    vim.api.nvim_create_augroup("lsp_document_highlight", {
-                        clear = false,
-                    })
-                    vim.api.nvim_clear_autocmds({
-                        group = "lsp_document_highlight",
-                        buffer = bufnr,
-                    })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        group = "lsp_document_highlight",
-                        buffer = bufnr,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
-                    vim.api.nvim_create_autocmd("CursorMoved", {
-                        group = "lsp_document_highlight",
-                        buffer = bufnr,
-                        callback = vim.lsp.buf.clear_references,
-                    })
-                end
+            if client.server_capabilities.documentSymbolProvider then
+                require("nvim-navic").attach(client, bufnr)
+                vim.opt_local.winbar = "%{%v:lua.require('nvim-navic').get_location()%}"
             end
 
-            local servers = {
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            runtime = { version = "LuaJIT" },
-                            workspace = { checkThirdParty = false },
-                            telemetry = { enable = false },
-                        },
+            -- Highlight references
+            if client.server_capabilities.documentHighlightProvider then
+                vim.api.nvim_create_augroup("lsp_document_highlight", {
+                    clear = false,
+                })
+                vim.api.nvim_clear_autocmds({
+                    group = "lsp_document_highlight",
+                    buffer = bufnr,
+                })
+                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                    group = "lsp_document_highlight",
+                    buffer = bufnr,
+                    callback = vim.lsp.buf.document_highlight,
+                })
+                vim.api.nvim_create_autocmd("CursorMoved", {
+                    group = "lsp_document_highlight",
+                    buffer = bufnr,
+                    callback = vim.lsp.buf.clear_references,
+                })
+            end
+        end
+
+        local servers = {
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        runtime = { version = "LuaJIT" },
+                        workspace = { checkThirdParty = false },
+                        telemetry = { enable = false },
                     },
                 },
-                clangd = {},
-                pyright = {},
-            }
+            },
+            clangd = {},
+            pyright = {},
+        }
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            for server, config in pairs(servers) do
-                lspconfig[server].setup(vim.tbl_deep_extend("force", {
-                    on_attach = on_attach,
-                    capabilities = vim.deepcopy(capabilities),
-                }, config))
-            end
-        end,
-    },
+        local lspconfig = require("lspconfig")
+        for server, config in pairs(servers) do
+            lspconfig[server].setup(vim.tbl_deep_extend("force", {
+                on_attach = on_attach,
+                capabilities = vim.deepcopy(capabilities),
+            }, config))
+        end
+    end,
 }
