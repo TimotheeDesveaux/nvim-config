74a574ac3a6057888b0851896ac36caee09ed66b
Timothee Desveaux <timothee.desveaux@gmail.com>
2023-06-12 09:56:27 +0200

    lsp(fidget): pin fidget to legacy version

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -18,6 +18,7 @@ return {
         },
         {
             "j-hui/fidget.nvim",
+            tag = "legacy",
             opts = {
                 text = {
                     spinner = "dots",
