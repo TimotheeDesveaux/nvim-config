2c769bef0620c2d16ad6ab706848ab4e553dafa2
Timothée Desveaux <timothee.desveaux@sirius-space.com>
2024-10-18 15:19:10 +0200

    snippets: remove tiger snippets

--- lua/custom/plugins/snippets/init.lua
+++ lua/custom/plugins/snippets/init.lua
@@ -45,7 +45,7 @@ return {
     config = function(_, opts)
         local ls = require("luasnip")
         ls.setup(opts)
-        for _, lang in pairs({ "c", "make", "markdown", "tiger", "sh", "nix" }) do
+        for _, lang in pairs({ "c", "make", "markdown", "sh", "nix" }) do
             ls.add_snippets(lang, require("custom.plugins.snippets." .. lang))
         end
         ls.add_snippets("cpp", require("custom.plugins.snippets.c"))
