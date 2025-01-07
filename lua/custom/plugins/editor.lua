ebbacff824c100cd2a7fd8dbd94e8599715ce497
Timothée Desveaux <timothee.desveaux@gmail.com>
2025-01-07 16:36:09 +0100

    editor: update telescope-fzf-native build options

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -22,7 +22,10 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             { "nvim-lua/plenary.nvim" },
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+            },
             { "nvim-tree/nvim-web-devicons" },
         },
         cmd = "Telescope",
