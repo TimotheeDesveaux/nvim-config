535e70441a8bab16caea287e4f1c64600650aac8
Timothée Desveaux <timothee.desveaux@gmail.com>
2025-01-07 16:36:09 +0100

    autocmds: move markup language autocmds to after/ftplugin

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,8 @@
+vim.opt_local.textwidth = 80
+vim.keymap.set("n", "<leader>pt", "<Cmd>!pandoc % -o %:r.pdf<CR>", { desc = "to pdf", buffer = 0 })
+vim.keymap.set(
+    "n",
+    "<leader>pv",
+    "<Cmd>silent !zathura %:r.pdf &<CR>",
+    { desc = "visualize", buffer = 0 }
+)
