ae372e59f97e6bc94be80178b699a7b77db92c6f
Timothée Desveaux <timothee.desveaux@gmail.com>
2025-12-08 10:36:13 +0100

    markdown: match textwitdth with new colorcolumn value (100)

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-vim.opt_local.textwidth = 80
+vim.opt_local.textwidth = 100
 vim.keymap.set("n", "<leader>pt", "<Cmd>!pandoc % -o %:r.pdf<CR>", { desc = "to pdf", buffer = 0 })
 vim.keymap.set(
     "n",
