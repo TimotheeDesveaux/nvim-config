ca444c9898b7ac4277fe77cba586fa93b21f6ad3
Timothée Desveaux <timothee.desveaux@gmail.com>
2026-01-13 10:31:28 +0100

    treesitter: move config to treesitter plugin configuration

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -54,6 +54,38 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         lazy = false,
+        config = function()
+            local augroup = require("custom.utils").augroup
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = augroup("treesitter"),
+                pattern = { "*" },
+                callback = function(event)
+                    local lang = vim.treesitter.language.get_lang(event.match)
+                    local is_installed = vim.treesitter.language.add(lang)
+
+                    if not is_installed then
+                        local ts = require("nvim-treesitter")
+                        local available_langs = ts.get_available()
+
+                        if vim.tbl_contains(available_langs, lang) then
+                            vim.notify(
+                                "Installing treesitter parser for " .. lang,
+                                vim.log.levels.INFO
+                            )
+                            ts.install({ lang }):wait(30 * 1000)
+                        end
+                    end
+
+                    local ok = pcall(vim.treesitter.start, event.buf, lang)
+                    if not ok then
+                        return
+                    end
+
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
     },
     {
         "stevearc/conform.nvim",
--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+function M.augroup(name, clear)
+    clear = clear ~= false
+    return vim.api.nvim_create_augroup("custom_" .. name, { clear = clear })
+end
+
 function M.telescope(builtin)
     return function()
         require("telescope.builtin")[builtin]()
