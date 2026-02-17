edc5955d55c6a8bc3bd6f2740d1c6059ebea1329
Timothée Desveaux <timothee.desveaux@gmail.com>
2026-02-17 17:33:49 +0100

    snippets: add rust snippets

---
+++ lua/custom/plugins/snippets/rust.lua
@@ -0,0 +1,9 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+    s("derive", fmt("#[derive({})]", { i(1) })),
+}
